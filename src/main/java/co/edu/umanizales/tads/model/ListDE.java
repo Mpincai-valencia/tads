497a585930911063bea57c142b1fb88902e7ff29
Mpincai-valencia <127371288+Mpincai-valencia@users.noreply.github.com>
2023-04-17 18:08:23 -0500

    Se creó el get para la cabeza

--- src/main/java/co/edu/umanizales/tads/model/ListDE.java
+++ src/main/java/co/edu/umanizales/tads/model/ListDE.java
@@ -8,7 +8,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ListDE {
-    private NodeDE head;
+    private NodeDE headDE;
     private int size;
+
+    public NodeDE getHead() {
+        return headDE;
+    }
+
 }
 
